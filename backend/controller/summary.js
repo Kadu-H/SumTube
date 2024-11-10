f7f035d96c068671dc198e84b3428e514a3ea8c5
Kdu <olocgame@gmail.com>
2024-11-10 18:04:53 -0300

    Mudando biblioteca que converte Markdown para PDF
    A biblioteca que estava, tinha varias vulnerabilidades!

--- backend/controller/summary.js
+++ backend/controller/summary.js
@@ -1,11 +1,10 @@
 import path, { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { unlink } from 'fs/promises';
-import markdownpdf from 'markdown-pdf';
+import { mdToPdf } from 'md-to-pdf';
 
 import { getTranscriptFromId } from '../model/transcript.js';
 import { getSummaryAI, getMarkdownAI } from '../model/gemini.js';
-import { error } from 'node:console';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -24,7 +23,7 @@ export const getSummary = async (req, res) => {
             transcript
         }
         return res.json(responseJson);
-    } catch (error) { 
+    } catch (error) {
         console.error(error);
         return res.redirect('/');
     }
@@ -33,19 +32,18 @@ export const getSummary = async (req, res) => {
 export const generatePdf = async (req, res) => {
     const { resumo, legenda, titulo } = req.body;
     const markdown = await getMarkdownAI(resumo, legenda, titulo);
-    const filePath = path.join(__dirname, `../../temp/${sanitizeFileName(titulo)}.pdf`)
-    markdownpdf()
-        .from.string(markdown)
-        .to(filePath, async () => { // Usando async dentro do callback
-            res.download(filePath, `${titulo}.pdf`, async (err) => {
-                if (err) {
-                    console.error('Erro ao fazer o download do PDF:', err);
-                } else {
-                    console.log(`PDF gerado com sucesso`);
-                    // Apaga o arquivo após o download
-                    await unlink(filePath);
-                    console.log(`Arquivo ${filePath} deletado com sucesso`);
-                }
-            });
+    const filePath = path.join(__dirname, `../../temp/${sanitizeFileName(titulo)}.pdf`);
+    const pdf = await mdToPdf({ content: markdown }, { dest: filePath });
+    if (pdf) {
+        res.download(filePath, `${titulo}.pdf`, async (err) => {
+            if (err) {
+                console.error('Erro ao fazer o download do PDF:', err);
+            } else {
+                console.log(`PDF gerado com sucesso`);
+                // Apaga o arquivo após o download
+                await unlink(filePath);
+                console.log(`Arquivo ${filePath} deletado com sucesso`);
+            }
         });
+    }
 }
